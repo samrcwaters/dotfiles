e63008d7eb801f7ad5e775b8ecc82bddc70b1127
Sam Waters <sam@fob.ag>
2026-02-08 14:35:01 -0800

    allow window names for txinit dirs

--- lib/tmux_utils.sh
+++ lib/tmux_utils.sh
@@ -1,16 +1,21 @@
 #!/bin/zsh
 
+# Create a new tmux session with windows for each directory in TMUX_DEFAULT_DIRS
 function txinit {
-  # Create a new tmux session with windows for each directory in TMUX_DEFAULT_DIRS
-  local first_dir="${TMUX_DEFAULT_DIRS[1]}"
+  local first_entry="${TMUX_DEFAULT_DIRS[1]}"
+  # Read this as "trim whatever matches `:*` from the **right** side of $first_entry".
+  # So "/some/dir:somedir_alias" becomes "/some/dir"
+  local first_dir="${first_entry%%:*}"
+  # Read this as "trim whatever matches `*:` from the left side of $first_entry".
+  local first_window_name="${first_entry##*:}"
   local session_name=$(basename "$first_dir")
-
   # Create the first window
-  tmux new-session -s "$session_name" -c "$first_dir" -d -n "$(basename "$first_dir")"
+  tmux new-session -s "$session_name" -c "$first_dir" -d -n $first_window_name
 
   # Create windows for remaining directories
-  for dir in "${TMUX_DEFAULT_DIRS[@]:1}"; do
-    local window_name=$(basename "$dir")
+  for entry in "${TMUX_DEFAULT_DIRS[@]:1}"; do
+    local dir="${entry%%:*}"
+    local window_name="${entry##*:}"
     tmux new-window -t "$session_name:" -c "$dir" -n "$window_name"
   done
 
@@ -19,8 +24,9 @@ function txinit {
   tmux attach-session -t "$session_name"
 }
 
+# Open a new tmux window in the current session with name based on target directory.
+# Unlike txinit, this does not currently support custom window names.
 function txopen {
-  # Open a new tmux window in the current session with name based on target directory
   local target_dir="${1:-$PWD}"
   target_dir="${target_dir/#\~/$HOME}"
   local window_name=$(basename "$target_dir")
